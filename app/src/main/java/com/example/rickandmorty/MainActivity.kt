91ac15fb96aebb0e3f7f5d6eeb6e38f8c5be1e2e
astro <ahmed123.as27@gmail.com>
2021-11-27 10:17:47 +0400

    created app UI

--- app/src/main/java/com/example/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.rickandmorty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
+import com.example.rickandmorty.databinding.ActivityMainBinding
+import java.io.IOException
+import java.lang.Exception
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
